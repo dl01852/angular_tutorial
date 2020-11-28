273304beec84112e24c27daf396e26dfd0cf71ed
David <davlewis2011@gmail.com>
2020-11-28 17:36:54 -0500

    first angular commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero'
+
+
+export const HEROES: Hero[] = [
+  {id: 11, surname: 'Iron Man'},
+  {id: 12, surname: 'Thor'},
+  {id: 13, surname: 'Captain America'},
+  {id: 14, surname: 'Black Widow'},
+  {id: 15, surname: 'Hulk'},
+  {id: 16, surname: 'Black Panther'},
+  {id: 17, surname: 'Clint Barton'},
+  {id: 18, surname: 'Dr. Strange'},
+  {id: 19, surname: 'SpiderMan'},
+  {id: 20, surname: 'Ant Man'}
+]
